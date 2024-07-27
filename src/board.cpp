e4415711bd7b973f1d45f5867dff6dd4f9106703
Tobias Wiedemann <towi00001@stud.uni-saarland.de>
2024-07-27 22:07:23 +0200

    Hybrid bitboard and 8x8 board

---
+++ src/board.cpp
@@ -0,0 +1,332 @@
+#include <bits/stdc++.h>
+#include <cassert>
+
+enum class Piece{
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    Queen,
+    King,
+    Empty
+};
+
+enum class Color{
+    White,
+    Black,
+    Empty
+};
+
+
+struct position {
+    position() {
+        piece_table.assign(64, Piece::Empty);
+        color_table.assign(64, Color::Empty);
+    }
+    // Bitboards
+    // White pieces
+    uint64_t white_pawns = 0ULL;
+    uint64_t white_knights = 0ULL;
+    uint64_t white_bishops = 0ULL;
+    uint64_t white_rooks = 0ULL;
+    uint64_t white_queens = 0ULL;
+    uint64_t white_kings = 0ULL;
+
+    // Black pieces
+    uint64_t black_pawns = 0ULL;
+    uint64_t black_knights = 0ULL;
+    uint64_t black_bishops = 0ULL;
+    uint64_t black_rooks = 0ULL;
+    uint64_t black_queens = 0ULL;
+    uint64_t black_kings = 0ULL;
+
+
+    // 8x8 Board
+    std::vector<Piece> piece_table;
+    std::vector<Color> color_table;
+
+    void set_piece(Piece piece, char file, int rank, Color col) {
+        int index = file - 'a' + 8 * (rank - 1);
+        assert(index >= 0);
+        assert(index < 64);
+        color_table[index] = col;
+
+        // Bitboard
+        if (col == Color::White) {
+            switch (piece) {
+                case Piece::Pawn:
+                    white_pawns = white_pawns | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Rook:
+                    white_rooks = white_rooks | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Knight:
+                    white_knights = white_knights | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Bishop:
+                    white_bishops = white_bishops | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Queen:
+                    white_queens = white_queens | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::King:
+                    white_kings = white_kings | (uint64_t) std::pow(2, index);
+                    break;
+                default:
+                    std::cout << "PIECE NOT FOUND";
+                    return;
+            }
+        } else {
+            switch (piece) {
+                case Piece::Pawn:
+                    black_pawns = black_pawns | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Rook:
+                    black_rooks = black_rooks | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Knight:
+                    black_knights = black_knights | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Bishop:
+                    black_bishops = black_bishops | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::Queen:
+                    black_queens = black_queens | (uint64_t) std::pow(2, index);
+                    break;
+                case Piece::King:
+                    black_kings = black_kings | (uint64_t) std::pow(2, index);
+                    break;
+                default:
+                    std::cout << "PIECE NOT FOUND";
+                    return;
+            }
+        }
+
+        // 8x8 Board
+        switch (piece) {
+            case Piece::Pawn:
+                piece_table[index] = Piece::Pawn;
+                break;
+            case Piece::Rook:
+                piece_table[index] = Piece::Rook;
+                break;
+            case Piece::Knight:
+                piece_table[index] = Piece::Knight;
+                break;
+            case Piece::Bishop:
+                piece_table[index] = Piece::Bishop;
+                break;
+            case Piece::Queen:
+                piece_table[index] = Piece::Queen;
+                break;
+            case Piece::King:
+                piece_table[index] = Piece::King;
+                break;
+            default:
+                std::cout << "PIECE NOT FOUND";
+                return;
+        }
+    }
+};
+
+void print_bitboard(uint64_t bitboard) {
+    for (int rank = 7; rank >= 0; --rank) {
+        for (int file = 0; file < 8; ++file) {
+            int square = rank * 8 + file;
+            if (bitboard & (1ULL << square)) {
+                std::cout << "1 ";
+            } else {
+                std::cout << ". ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+void print_all_bitboards(position &p) {
+
+    std::cout << "white pawns\n";
+    print_bitboard(p.white_pawns);
+    std::cout << "white knights\n";
+    print_bitboard(p.white_knights);
+    std::cout << "white bishops\n";
+    print_bitboard(p.white_bishops);
+    std::cout << "white rooks\n";
+    print_bitboard(p.white_rooks);
+    std::cout << "white queen\n";
+    print_bitboard(p.white_queens);
+    std::cout << "white king\n";
+    print_bitboard(p.white_kings);
+
+    std::cout << "black pawns\n";
+    print_bitboard(p.black_pawns);
+    std::cout << "black knights\n";
+    print_bitboard(p.black_knights);
+    std::cout << "black bishops\n";
+    print_bitboard(p.black_bishops);
+    std::cout << "black rooks\n";
+    print_bitboard(p.black_rooks);
+    std::cout << "black queen\n";
+    print_bitboard(p.black_queens);
+    std::cout << "black king\n";
+    print_bitboard(p.black_kings);
+}
+
+void print_full_board(position &p) {
+    std::cout << "\nBitboard-based\n";
+    for (int rank = 7; rank >= 0; --rank) {
+        for (int file = 0; file < 8; ++file) {
+            int square = rank * 8 + file;
+            if (p.black_pawns & (1ULL << square)) {
+                std::cout << "p ";
+            } else if (p.black_knights & (1ULL << square)) {
+                std::cout << "n ";
+            } else if (p.black_bishops & (1ULL << square)) {
+                std::cout << "b ";
+            } else if (p.black_rooks & (1ULL << square)) {
+                std::cout << "r ";
+            } else if (p.black_queens & (1ULL << square)) {
+                std::cout << "q ";
+            } else if (p.black_kings & (1ULL << square)) {
+                std::cout << "k ";
+ 
+            } else if (p.white_pawns & (1ULL << square)) {
+                std::cout << "P ";
+            } else if (p.white_knights & (1ULL << square)) {
+                std::cout << "N ";
+            } else if (p.white_bishops & (1ULL << square)) {
+                std::cout << "B ";
+            } else if (p.white_rooks & (1ULL << square)) {
+                std::cout << "R ";
+            } else if (p.white_queens & (1ULL << square)) {
+                std::cout << "Q ";
+            } else if (p.white_kings & (1ULL << square)) {
+                std::cout << "K ";
+            } else {
+                std::cout << ". ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "\n8x8-based\n";
+    for (int rank = 7; rank >= 0; --rank) {
+        for (int file = 0; file < 8; ++file) {
+            int index = rank * 8 + file;
+            assert(index >= 0);
+            assert(index < 64);
+            auto piece = p.piece_table[index];
+            char res;
+            switch (piece) {
+                case Piece::Pawn:
+                    res = 'p';
+                    break;
+                case Piece::Rook:
+                    res = 'r';
+                    break;
+                case Piece::Knight:
+                    res = 'n';
+                    break;
+                case Piece::Bishop:
+                    res = 'b';
+                    break;
+                case Piece::Queen:
+                    res = 'q';
+                    break;
+                case Piece::King:
+                    res = 'k';
+                    break;
+                case Piece::Empty:
+                    res = '.';
+                    break;
+                default:
+                    std::cout << "PANIC, invalid piece in 8x8\n";
+                    return;
+            }
+            if (p.color_table[index] == Color::White) {
+                res -= 32;
+            }
+            std::cout << res << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+position starting_bitboards() {
+    position p;
+    // White pieces
+    p.white_pawns = 0x000000000000FF00ULL;
+    p.white_knights = 0x0000000000000042ULL;
+    p.white_bishops = 0x0000000000000024ULL;
+    p.white_rooks = 0x0000000000000081ULL;
+    p.white_queens = 0x0000000000000008ULL;
+    p.white_kings = 0x0000000000000010ULL;
+
+    // Black pieces
+    p.black_pawns = 0x00FF000000000000ULL;
+    p.black_knights = 0x4200000000000000ULL;
+    p.black_bishops = 0x2400000000000000ULL;
+    p.black_rooks = 0x8100000000000000ULL;
+    p.black_queens = 0x0800000000000000ULL;
+    p.black_kings = 0x1000000000000000ULL;
+
+    return p;
+}
+
+position &starting_position(position& p) {
+
+    p.set_piece(Piece::Pawn, 'a', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'b', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'c', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'd', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'e', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'f', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'g', 2, Color::White);
+    p.set_piece(Piece::Pawn, 'h', 2, Color::White);
+
+    p.set_piece(Piece::Rook, 'a', 1, Color::White);
+    p.set_piece(Piece::Rook, 'h', 1, Color::White);
+
+    p.set_piece(Piece::Knight, 'b', 1, Color::White);
+    p.set_piece(Piece::Knight, 'g', 1, Color::White);
+
+    p.set_piece(Piece::Bishop, 'c', 1, Color::White);
+    p.set_piece(Piece::Bishop, 'f', 1, Color::White);
+
+    p.set_piece(Piece::Queen, 'd', 1, Color::White);
+
+    p.set_piece(Piece::King, 'e', 1, Color::White);
+
+
+    p.set_piece(Piece::Pawn, 'a', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'b', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'c', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'd', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'e', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'f', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'g', 7, Color::Black);
+    p.set_piece(Piece::Pawn, 'h', 7, Color::Black);
+
+    p.set_piece(Piece::Rook, 'a', 8, Color::Black);
+    p.set_piece(Piece::Rook, 'h', 8, Color::Black);
+
+    p.set_piece(Piece::Knight, 'b', 8, Color::Black);
+    p.set_piece(Piece::Knight, 'g', 8, Color::Black);
+
+    p.set_piece(Piece::Bishop, 'c', 8, Color::Black);
+    p.set_piece(Piece::Bishop, 'f', 8, Color::Black);
+
+    p.set_piece(Piece::Queen, 'd', 8, Color::Black);
+
+    p.set_piece(Piece::King, 'e', 8, Color::Black);
+
+    return p;
+}
+
+int main() {
+    position p;
+    position s = starting_position(p);
+    print_full_board(s);
+
+
+    return 0;
+}
